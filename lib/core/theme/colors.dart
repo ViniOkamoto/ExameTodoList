4a8ee8c6ee4b2a12383cb34b785a556109aa1750
Vinicius Okamoto <leo_kamoto@hotmail.com>
2021-07-25 20:57:23 -0300

    refact(Colors): adjusted app colors

--- lib/core/theme/colors.dart
+++ lib/core/theme/colors.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
-class PubliColors {
-  static const Color highlightDarkColor = Color(0xFF2494C5);
-  static const Color highlightColor = Color(0xFF28A8E0);
-  static const Color highlightLightColor = Color(0xFFB4E7FD);
+class TodoColors {
+  static const Color highlightDarkColor = Color(0xFFD4780E);
+  static const Color highlightColor = Color(0xFFFB8B0B);
+  static const Color highlightLightColor = Color(0xFFFFB560);
   static const Color darkestColor = Color(0xFF2A3740);
   static const Color darkerColor = Color(0xFF4C5667);
   static const Color darkColor = Color(0xFF697A8B);
   static const Color grayColor = Color(0xFFA7AFBB);
-  static const Color lightestColor = Color(0xFFF4F7FF);
+  static const Color lightestColor = Color(0xFFF6F7FF);
   static const Color lighterColor = Color(0xFFE5EBF0);
   static const Color lightColor = Color(0xFFD0D8DE);
   static const Color whiteColor = Color(0xFFFFFFFF);
-  static const Color greenProgressColor = Color(0xFF5FF46E);
-  static const Color yellowProgressColor = Color(0xFFFF8E3C);
-  static const Color redProgressColor = Color(0xFFF45F5F);
+  static const Color greenPriorityColor = Color(0xFF5FF46E);
+  static const Color yellowPriorityColor = Color(0xFFFFEF60);
+  static const Color redPriorityColor = Color(0xFFF45F5F);
 }
