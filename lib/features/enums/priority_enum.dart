9e53b24378c8f9a7ad704fffcf98b28e122eda66
Vinicius Okamoto <leo_kamoto@hotmail.com>
2021-07-25 20:58:13 -0300

    feat(Priority): added priority colors

--- lib/features/enums/priority_enum.dart
+++ lib/features/enums/priority_enum.dart
@@ -1,5 +1,14 @@
+import 'package:exame_todo_list/core/theme/colors.dart';
+import 'package:flutter/material.dart';
+
 enum TodoPriority {
   highPriority,
   mediumPriority,
   lowPriority,
 }
+
+const Map<TodoPriority, Color> priorityColor = {
+  TodoPriority.highPriority: TodoColors.redPriorityColor,
+  TodoPriority.mediumPriority: TodoColors.yellowPriorityColor,
+  TodoPriority.lowPriority: TodoColors.greenPriorityColor,
+};
