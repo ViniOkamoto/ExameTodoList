43fa75ae25b8db2b592146666130a909beb4262e
Vinicius Okamoto <leo_kamoto@hotmail.com>
2021-07-25 05:05:16 -0300

    feat(TodoRepositoryTest): added more crud scenarios

--- test/features/repositories/todo_repository_test.dart
+++ test/features/repositories/todo_repository_test.dart
@@ -3,6 +3,7 @@ import 'package:exame_todo_list/features/datasources/todo_local_data_source.dart
 import 'package:exame_todo_list/features/enums/priority_enum.dart';
 import 'package:exame_todo_list/features/models/todo.dart';
 import 'package:exame_todo_list/features/repositories/todo_repository.dart';
+import 'package:exame_todo_list/features/state/todo_creation_state.dart';
 import 'package:exame_todo_list/features/state/todo_state.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
@@ -58,59 +59,157 @@ void main() {
   group(
     "Todo repository tests",
     () {
-      test(
-        "Should return TodoState without exception and returning from local source a task list",
-        () async {
-          when(localDataSource.getTaskList()).thenAnswer(
-            (_) async => mockTaskList,
+      group("get methods", () {
+        test(
+          "Should return TodoState without exception and returning from local source a task list",
+          () async {
+            when(localDataSource.getTaskList()).thenAnswer(
+              (_) async => mockTaskList,
+            );
+
+            final result = await repository.getTaskList();
+
+            expect(result, mockSuccessfulState);
+            verify(localDataSource.getTaskList()).called(1);
+            verifyNoMoreInteractions(localDataSource);
+          },
+        );
+
+        test(
+          "Should return TodoState without exception and with task list empty",
+          () async {
+            when(localDataSource.getTaskList()).thenAnswer(
+              (_) async => <Todo>[],
+            );
+
+            final result = await repository.getTaskList();
+
+            expect(result, mockSuccessfulStateWithEmptyList);
+            verify(localDataSource.getTaskList());
+            verifyNoMoreInteractions(localDataSource);
+          },
+        );
+
+        test(
+          "Should return TodoState with local exception from external source and return a LocalFailure exception inside the state",
+          () async {
+            when(localDataSource.getTaskList()).thenThrow(LocalCacheException(errorText: "local error"));
+
+            final result = await repository.getTaskList();
+
+            expect(result, mockLocalErrorState);
+            verify(localDataSource.getTaskList());
+            verifyNoMoreInteractions(localDataSource);
+          },
+        );
+
+        test(
+          "Should return TodoState with any exception and return a Failure exception inside the state",
+          () async {
+            when(localDataSource.getTaskList()).thenThrow(Exception());
+
+            final result = await repository.getTaskList();
+
+            expect(result, mockAnyErrorState);
+            verify(localDataSource.getTaskList());
+            verifyNoMoreInteractions(localDataSource);
+          },
+        );
+      });
+
+      group(
+        "create scenarios",
+        () {
+          test(
+            "Should return TodoCreation without exception and verify methods to create todo",
+            () async {
+              final result = await repository.createTask(mockTodoModel);
+
+              expect(result, TodoCreationState.success());
+              verify(localDataSource.createTask(mockTodoModel));
+              verifyNoMoreInteractions(localDataSource);
+            },
           );
 
-          final result = await repository.getTaskList();
+          test(
+            "Should return TodoCreation with Failure exception",
+            () async {
+              when(localDataSource.createTask(mockTodoModel)).thenThrow(Exception());
+              final result = await repository.createTask(mockTodoModel);
 
-          expect(result, mockSuccessfulState);
-          verify(localDataSource.getTaskList()).called(1);
-          verifyNoMoreInteractions(localDataSource);
-        },
-      );
-      //
-      test(
-        "Should return TodoState without exception and with task list empty",
-        () async {
-          when(localDataSource.getTaskList()).thenAnswer(
-            (_) async => <Todo>[],
+              expect(result, TodoCreationState.error(error: Failure()));
+              verify(localDataSource.createTask(mockTodoModel));
+              verifyNoMoreInteractions(localDataSource);
+            },
           );
 
-          final result = await repository.getTaskList();
+          test(
+            "Should return TodoCreation with LocalFailure exception",
+            () async {
+              when(localDataSource.createTask(mockTodoModel)).thenThrow(LocalCacheException(errorText: "erro"));
+              final result = await repository.createTask(mockTodoModel);
 
-          expect(result, mockSuccessfulStateWithEmptyList);
-          verify(localDataSource.getTaskList());
-          verifyNoMoreInteractions(localDataSource);
+              expect(result, TodoCreationState.error(error: LocalFailure(errorText: "erro")));
+              verify(localDataSource.createTask(mockTodoModel));
+              verifyNoMoreInteractions(localDataSource);
+            },
+          );
         },
       );
 
-      test(
-        "Should return TodoState with local exception from external source and return a LocalFailure exception inside the state",
-        () async {
-          when(localDataSource.getTaskList()).thenThrow(LocalCacheException(errorText: "local error"));
+      group(
+        "delete scenarios",
+        () {
+          test(
+            "Should return TodoCreation without exception and verify methods to delete todo",
+            () async {
+              final result = await repository.deleteTask(mockTodoModel);
 
-          final result = await repository.getTaskList();
+              expect(result, TodoCreationState.success());
+              verify(localDataSource.deleteTask(mockTodoModel));
+              verifyNoMoreInteractions(localDataSource);
+            },
+          );
 
-          expect(result, mockLocalErrorState);
-          verify(localDataSource.getTaskList());
-          verifyNoMoreInteractions(localDataSource);
+          test(
+            "Should return TodoCreation with Failure exception",
+            () async {
+              when(localDataSource.deleteTask(mockTodoModel)).thenThrow(Exception());
+              final result = await repository.deleteTask(mockTodoModel);
+
+              expect(result, TodoCreationState.error(error: Failure()));
+              verify(localDataSource.deleteTask(mockTodoModel));
+              verifyNoMoreInteractions(localDataSource);
+            },
+          );
         },
       );
 
-      test(
-        "Should return TodoState with any exception and return a Failure exception inside the state",
-        () async {
-          when(localDataSource.getTaskList()).thenThrow(Exception());
+      group(
+        "update scenarios",
+        () {
+          test(
+            "Should return TodoCreation without exception and verify methods to update todo",
+            () async {
+              final result = await repository.updateTask(mockTodoModel);
 
-          final result = await repository.getTaskList();
+              expect(result, TodoCreationState.success());
+              verify(localDataSource.updateTask(mockTodoModel));
+              verifyNoMoreInteractions(localDataSource);
+            },
+          );
 
-          expect(result, mockAnyErrorState);
-          verify(localDataSource.getTaskList());
-          verifyNoMoreInteractions(localDataSource);
+          test(
+            "Should return TodoCreation with Failure exception",
+            () async {
+              when(localDataSource.updateTask(mockTodoModel)).thenThrow(Exception());
+              final result = await repository.updateTask(mockTodoModel);
+
+              expect(result, TodoCreationState.error(error: Failure()));
+              verify(localDataSource.updateTask(mockTodoModel));
+              verifyNoMoreInteractions(localDataSource);
+            },
+          );
         },
       );
     },
