d47036ba7eed008e0c4f96cded71ea0a7acfee37
Vinicius Okamoto <leo_kamoto@hotmail.com>
2021-07-25 20:56:32 -0300

    feat(Yaml): added fonts and build config

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:exame_todo_list/core/services/hive/hive_service.dart';
 import 'package:exame_todo_list/todo_list_app.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await HiveService.init();
-
-  runApp(TodoListApp());
+  Intl.defaultLocale = 'pt_BR';
+  initializeDateFormatting('pt_BR', null).then((value) => runApp(TodoListApp()));
 }
