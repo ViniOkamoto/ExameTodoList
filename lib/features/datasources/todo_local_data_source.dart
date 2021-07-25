cb56154e439aa25932ef896478d39360ec7c48ca
Vinicius Okamoto <leo_kamoto@hotmail.com>
2021-07-25 05:38:47 -0300

    feat(TodoLocalDataSource): added create scenario

--- lib/features/datasources/todo_local_data_source.dart
+++ lib/features/datasources/todo_local_data_source.dart
@@ -4,6 +4,9 @@ import 'package:exame_todo_list/features/models/todo.dart';
 
 abstract class TodoLocalDataSource {
   Future<List<Todo>> getTaskList();
+  Future<void> createTask(Todo todo);
+  Future<void> updateTask(Todo todo);
+  Future<void> deleteTask(Todo todo);
 }
 
 class TodoLocalDataSourceImplementation implements TodoLocalDataSource {
@@ -19,7 +22,38 @@ class TodoLocalDataSourceImplementation implements TodoLocalDataSource {
 
       return todoList;
     } on LocalCacheException catch (e) {
+      throw LocalFailure(errorText: e.errorText);
+    } on Exception catch (e) {
       throw e;
+    }
+  }
+
+  @override
+  Future<void> deleteTask(Todo todo) async {
+    try {
+      await todo.delete();
+    } on Exception catch (e) {
+      throw e;
+    }
+  }
+
+  @override
+  Future<void> createTask(Todo todo) async {
+    try {
+      final box = await hive.openBox(typeString: 'todo');
+
+      box.add(todo);
+    } on LocalCacheException catch (e) {
+      throw LocalFailure(errorText: e.errorText);
+    } on Exception catch (e) {
+      throw e;
+    }
+  }
+
+  @override
+  Future<void> updateTask(Todo todo) async {
+    try {
+      await todo.save();
     } on Exception catch (e) {
       throw e;
     }
--- test/features/datasource/todo_local_data_source_test.dart
+++ test/features/datasource/todo_local_data_source_test.dart
@@ -36,76 +36,137 @@ void main() {
   ];
 
   group(
-    "Todo repository tests",
+    "Todo datasource tests",
     () {
-      test(
-        "Should open and return hive box and return a task list with 3 elements",
-        () async {
-          when(mockHive.openBox(typeString: "todo")).thenAnswer(
-            (_) async => mockBox,
-          );
-          when(mockBox.values.toList()).thenAnswer(
-            (_) => mockTaskList,
+      group(
+        "get scenarios",
+        () {
+          test(
+            "Should open and return hive box and return a task list with 3 elements",
+            () async {
+              when(mockHive.openBox(typeString: "todo")).thenAnswer(
+                (_) async => mockBox,
+              );
+              when(mockBox.values.toList()).thenAnswer(
+                (_) => mockTaskList,
+              );
+
+              final result = await localDataSource.getTaskList();
+
+              expect(result.length, 3);
+              verify(mockHive.openBox(typeString: "todo")).called(1);
+              verify(mockBox.values).called(1);
+              verifyNoMoreInteractions(mockBox);
+              verifyNoMoreInteractions(mockHive);
+            },
           );
 
-          final result = await localDataSource.getTaskList();
+          test(
+            "Should open and return hive box and return a empty task list",
+            () async {
+              when(mockHive.openBox(typeString: "todo")).thenAnswer(
+                (_) async => mockBox,
+              );
+              when(mockBox.values.toList()).thenAnswer(
+                (_) => <Todo>[],
+              );
 
-          expect(result.length, 3);
-          verify(mockHive.openBox(typeString: "todo")).called(1);
-          verify(mockBox.values).called(1);
-          verifyNoMoreInteractions(mockBox);
-          verifyNoMoreInteractions(mockHive);
+              final result = await localDataSource.getTaskList();
+
+              expect(result.length, 0);
+              verify(mockHive.openBox(typeString: "todo")).called(1);
+              verify(mockBox.values.toList()).called(1);
+              verifyNoMoreInteractions(mockBox);
+              verifyNoMoreInteractions(mockHive);
+            },
+          );
+
+          test(
+            "Should throw LocalCacheException when don't found hive box",
+            () async {
+              when(mockHive.openBox(typeString: "todo")).thenThrow(LocalFailure(errorText: "Erro cache"));
+              final result = localDataSource.getTaskList();
+
+              expect(() => result, throwsA(isA<LocalFailure>()));
+              verify(mockHive.openBox(typeString: "todo")).called(1);
+              verifyNever(mockBox.values);
+              verifyNoMoreInteractions(mockBox);
+              verifyNoMoreInteractions(mockHive);
+            },
+          );
+
+          test(
+            "Should capture any exception when try to get list",
+            () async {
+              when(mockHive.openBox(typeString: "todo")).thenAnswer(
+                (_) async => mockBox,
+              );
+
+              when(mockBox.values.toList()).thenThrow(Exception());
+              final result = localDataSource.getTaskList();
+
+              expect(() => result, throwsA(isA<Exception>()));
+              verify(mockHive.openBox(typeString: "todo")).called(1);
+              verifyNoMoreInteractions(mockBox);
+              verifyNoMoreInteractions(mockHive);
+            },
+          );
         },
       );
 
-      test(
-        "Should open and return hive box and return a empty task list",
-        () async {
-          when(mockHive.openBox(typeString: "todo")).thenAnswer(
-            (_) async => mockBox,
-          );
-          when(mockBox.values.toList()).thenAnswer(
-            (_) => <Todo>[],
-          );
+      group(
+        "create scenarios",
+        () {
+          test(
+            "Should create task and and verify methods for creating task",
+            () async {
+              when(mockHive.openBox(typeString: "todo")).thenAnswer(
+                (_) async => mockBox,
+              );
+              when(mockBox.add(mockTodoModel)).thenAnswer((_) async {
+                return 0;
+              });
 
-          final result = await localDataSource.getTaskList();
+              await localDataSource.createTask(mockTodoModel);
 
-          expect(result.length, 0);
-          verify(mockHive.openBox(typeString: "todo")).called(1);
-          verify(mockBox.values.toList()).called(1);
-          verifyNoMoreInteractions(mockBox);
-          verifyNoMoreInteractions(mockHive);
-        },
-      );
-
-      test(
-        "Should throw LocalCacheException when don't found hive box",
-        () async {
-          when(mockHive.openBox(typeString: "todo")).thenThrow(LocalCacheException(errorText: "Erro cache"));
-          final result = localDataSource.getTaskList();
-
-          expect(() => result, throwsA(isA<LocalCacheException>()));
-          verify(mockHive.openBox(typeString: "todo")).called(1);
-          verifyNever(mockBox.values);
-          verifyNoMoreInteractions(mockBox);
-          verifyNoMoreInteractions(mockHive);
-        },
-      );
-
-      test(
-        "Should capture any exception when try to get list",
-        () async {
-          when(mockHive.openBox(typeString: "todo")).thenAnswer(
-            (_) async => mockBox,
+              verify(mockHive.openBox(typeString: "todo")).called(1);
+              verify(mockBox.add(mockTodoModel)).called(1);
+              verifyNoMoreInteractions(mockBox);
+              verifyNoMoreInteractions(mockHive);
+            },
           );
 
-          when(mockBox.values.toList()).thenThrow(Exception());
-          final result = localDataSource.getTaskList();
+          test(
+            "Should throw LocalCacheException when don't found hive box",
+            () async {
+              when(mockHive.openBox(typeString: "todo")).thenThrow(LocalFailure(errorText: "Erro cache"));
+              final result = localDataSource.createTask(mockTodoModel);
 
-          expect(() => result, throwsA(isA<Exception>()));
-          verify(mockHive.openBox(typeString: "todo")).called(1);
-          verifyNoMoreInteractions(mockBox);
-          verifyNoMoreInteractions(mockHive);
+              expect(() => result, throwsA(isA<LocalFailure>()));
+              verify(mockHive.openBox(typeString: "todo")).called(1);
+              verifyNever(mockBox.add(mockTodoModel));
+              verifyNoMoreInteractions(mockBox);
+              verifyNoMoreInteractions(mockHive);
+            },
+          );
+
+          test(
+            "Should capture any exception when call box.add",
+            () async {
+              when(mockHive.openBox(typeString: "todo")).thenAnswer(
+                (_) async => mockBox,
+              );
+              when(mockBox.add(mockTodoModel)).thenThrow(Exception());
+
+              final result = localDataSource.createTask(mockTodoModel);
+
+              expect(() => result, throwsA(isA<Exception>()));
+              verify(mockHive.openBox(typeString: "todo")).called(1);
+              verifyNever(mockBox.add(mockTodoModel));
+              verifyNoMoreInteractions(mockBox);
+              verifyNoMoreInteractions(mockHive);
+            },
+          );
         },
       );
     },
